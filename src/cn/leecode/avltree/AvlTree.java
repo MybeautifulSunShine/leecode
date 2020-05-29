14a7d9c2baf8c54c9e21e5e6618f4167442fb9fe
hegaojian <m18234818170@163.com>
2020-05-29 18:27:23 +0800

    AVLtree 创建以及理解

--- src/cn/leecode/avltree/AvlTree.java
+++ src/cn/leecode/avltree/AvlTree.java
@@ -67,8 +67,8 @@ public class AvlTree<E> extends BST<E> {
         //判断 LL RR LR RL
         //p是 g这个节点中左右最高的节点
         //n 是p 最右子树高度里面最高的
-        Node<E> parent = ((AVLNode<E>) grand).tollerChild();
-        Node<E> node = ((AVLNode<E>) grand).tollerChild();
+        Node<E> parent = ((AVLNode<E>) grand).tallerChild();
+        Node<E> node = ((AVLNode<E>) grand).tallerChild();
         //旋转方向的判断
         //L
         if (parent.isLeftChild()) {
@@ -92,6 +92,91 @@ public class AvlTree<E> extends BST<E> {
                 rotateLeft(grand);
             }
         }
+    }
+
+    /**
+     * version2
+     *
+     * @param grand
+     */
+    private void rebalance2(Node<E> grand) {
+        //判断 LL RR LR RL
+        //p是 g这个节点中左右最高的节点
+        //n 是p 最右子树高度里面最高的
+        Node<E> parent = ((AVLNode<E>) grand).tallerChild();
+        Node<E> node = ((AVLNode<E>) grand).tallerChild();
+        //旋转方向的判断
+        //L
+        if (parent.isLeftChild()) {
+            //判断子节点是否是左节点
+            if (node.isLeftChild()) {
+                //LL 对g进行右旋转
+                rotate(grand, node.left, node, node.right, parent, parent.right, grand, grand.right);
+            } else {
+                //LR
+                rotate(grand, parent.left, parent, node.left, node, node.right, grand, grand.right);
+            }
+        } else {
+            //R
+            if (node.isLeftChild()) {
+                //RL 先对parent进行右旋转
+                rotate(grand, grand.left, grand, node.left, node, node.right, parent, parent.right);
+            } else {
+                //RR 左旋转
+                rotate(grand, grand.left, grand, parent.left, parent, node.left, node, node.right);
+            }
+        }
+    }
+
+    /**
+     * 统一的旋转 对应的参数
+     */
+    private void rotate(Node<E> r, // 子树的根节点 也就是之前的跟节点
+                        Node<E> a, Node<E> b, Node<E> c,
+                        Node<E> d,
+                        Node<E> e, Node<E> f, Node<E> g) {
+        //让d成为这个子树的根结点
+        d.parent = r.parent;
+        if (r.isLeftChild()) {
+            r.parent.left = d;
+        } else if (r.isRightChild()) {
+            r.parent.right = d;
+        } else {
+            root = d;
+        }
+        //处理ABC节点
+        b.left = a;
+        if (a != null) {
+            a.parent = b;
+        }
+        b.right = c;
+        if (c != null) {
+            c.parent = b;
+        }
+        //更新高度是让B的高度做出了修改
+        updateHeight(b);
+
+        //处理E,f,g
+        //处理ABC节点
+        f.left = e;
+        if (e != null) {
+            e.parent = b;
+        }
+        f.right = g;
+        if (g != null) {
+            g.parent = b;
+        }
+        updateHeight(f);
+
+        //左边和右边都搞定了
+        //处理b ,d ,f
+        d.left = b;
+        d.right = f;
+        b.parent = d;
+        f.parent = d;
+        updateHeight(d);
+
+        //d f b 主要的空的
     }
 
     /**
@@ -207,10 +292,12 @@ public class AvlTree<E> extends BST<E> {
             height = 1 + Math.max(leftHeight, rightHeight);
         }
 
-
-        public Node<E> tollerChild() {
+        /**
+         * 返回节点比较高的节点
+         */
+        public Node<E> tallerChild() {
             //强制类型装换
-            int leftHeight = left == null ? 0 : ((AVLNode<E>) right).height;
+            int leftHeight = left == null ? 0 : ((AVLNode<E>) left).height;
             //右子树的高度
             int rightHeight = right == null ? 0 : ((AVLNode<E>) right).height;
             //那边高返回那个节点
@@ -222,6 +309,15 @@ public class AvlTree<E> extends BST<E> {
             }
             //如果相等统一返回左节点
             return isLeftChild() ? left : right;
+        }
+
+        @Override
+        public String toString() {
+            String parentString = "null";
+            if (parent != null) {
+                parentString = parent.element.toString();
+            }
+            return element + "_p(" + parentString + ")_h(" + height + ")";
         }
     }
 }
