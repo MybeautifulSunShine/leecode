7d090168c243a23bcd521c996c4965854905376c
hegaojian <m18234818170@163.com>
2020-08-14 18:40:45 +0800

    代码复习调整--循环队列理解

--- src/cn/hgj/sort/cmp/InsertionSort2.java
+++ src/cn/hgj/sort/cmp/InsertionSort2.java
@@ -11,6 +11,42 @@ package cn.hgj.sort.cmp;
 public class InsertionSort2<T extends Comparable<T>> extends Sort<T> {
     @Override
     protected void sort() {
+        for (int begin = 1; begin < array.length; begin++) {
+            insert(begin, search(begin));
+        }
+    }
 
+    /**
+     * 利用二分搜索找到 index 位置元素的待插入位置
+     * 已经排好序数组的区间范围是 [0, index)
+     *
+     * @param index
+     * @return
+     */
+    private int search(int index) {
+        int begin = 0;
+        int end = index;
+        while (begin < end) {
+            int mid = (begin + end) >> 1;
+            if (cmp(array[index], array[mid]) < 0) {
+                end = mid;
+            } else {
+                begin = mid + 1;
+            }
+        }
+        return begin;
+    }
+
+    /**
+     * 将source位置的元素插入到dest位置
+     *
+     * @param source index的位置
+     */
+    private void insert(int source, int dest) {
+        T v = array[source];
+        for (int i = source; i > dest; i--) {
+            array[i] = array[i - 1];
+        }
+        array[dest] = v;
     }
 }
